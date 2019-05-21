b5c1684ae4e3327fcc77bfdbd27895d47596f077
whd4607@naver.com <whd4607@naver.com>
2019-05-21 19:45:44 +0900

    ADD C Project

---
+++ 6장 02번/02번.c
@@ -0,0 +1,17 @@
+/*두 정수가 약수인지 서로소인지 구별*/
+
+#include<stdio.h>
+int main()
+{
+	int x, y;
+	printf("정수를 입력하시오 : ");
+	scanf_s("%d", &x);
+	printf("정수를 입력하시오 : ");
+	scanf_s("%d", &y);
+
+	if (x%y == 0)
+		printf("약수입니다.");
+	else
+		printf("서로소입니다.");
+	return 0;
+}
