d6f5ef9bb5fbad22a35ebcd2c16da18086a2571f
whd4607@naver.com <whd4607@naver.com>
2019-07-21 23:07:26 +0900

    ADD C Project

--- 7월16일/10장/16-2번/16-2번.c
+++ 7월16일/10장/16-2번/16-2번.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
+#include<time.h>
 
 void print(char arr[15][15]);
 void reset_map(char arr[15][15]);
-void mon_start(char monster[15][15], char mon);
+void mon_start(char monster1[15][15], char monster2[15][15], char mon1, char mon2);
 int find_row(char arr[15][15], char target);
 int find_col(char arr[15][15], char target);
 void move_hero(char hero[15][15], int row_hero, int col_hero, char hero1);
@@ -22,8 +23,7 @@ int main() {
 	reset_map(arr);
 
 	hero[0][0] = hero1;		//주인공 초기위치
-	mon_start(monster1, mon1);		//몬스터1 초기위치
-	mon_start(monster2, mon2);		//몬스터2 초기위치
+	mon_start(monster1, monster1, mon1, mon2);		//몬스터1, 2 초기위치
 	
 	row_hero = find_row(hero, hero1);		//주인공 행 좌표
 	col_hero = find_col(hero, hero1);		//주인공 열 좌표
@@ -97,17 +97,21 @@ void reset_map(char arr[15][15]) {
 	arr[14][14] = 'G';		//오른쪽 맨밑 = 금 위치
 }
 
-void mon_start(char monster[15][15], char mon) {
-	int n, m;
+void mon_start(char monster1[15][15], char monster2[15][15], char mon1, char mon2) {
+	int n1, m1, n2, m2;
 	while (1) {
-		n = rand() % 15;
-		m = rand() % 15;
-		if (n > 3 && n < 15 && n != 15 && m != 15)		//주인공의 시작점에서 4 이상 떨어져있어야하고 금과 같은 위치에 있으면 안된다.
+		srand((unsigned)time(NULL));
+		n1 = rand() % 15;
+		m1 = rand() % 15;
+		n2 = rand() % 15;
+		m2 = rand() % 15;
+		if (n1 != 14 || m1 != 14 && n2 != 14 || m2 != 14)		//몬스터는 금이 있는 자리에 갈 수 없다
 			break;
-		if (m > 3 && m < 15 && n != 15 && m != 15)		//주인공의 시작점에서 4 이상 떨어져있어야하고 금과 같은 위치에 있으면 안된다.
+		if (n1 < 3 || m1 < 3 || n2 < 3 || m2 < 3)		//몬스터의 초기 위치는 유저와 3의 거리 이상 떨어져 있어야 한다.
 			break;
 	}
-	monster[n][m]=mon;
+	monster1[n1][m1] = mon1;
+	monster2[n2][m2] = mon2;
 }
 
 int find_row(char arr[15][15], char target) {
@@ -130,9 +134,12 @@ int find_col(char arr[15][15], char target) {
 
 void move_hero(char hero[15][15], int row_hero, int col_hero, char hero1) {
 	int key;
+	//지금 여기 보면 값 받아오기만 하고 바뀐 좌표값을 저장하는게 없음 지역변수로 받아서 메인에 적용 안되는거 같은데
+	//row col 변수가 받아오고 변경은 되는데 함수 안에서만 바뀌는거 같음
 
 	row_hero = find_row(hero, hero1);		//주인공 행 좌표
 	col_hero = find_col(hero, hero1);		//주인공 열 좌표
+	//함수로 만들때는 값이 복사되서 쓰이기만하는거였나 그럼 바뀐값을 저장하던가 반환해야겠네	
 	while (1) {
 		key = _getch();
 		if (key != 'W' && key != 'A' && key != 'S' && key != 'D' && key != 'w' && key != 'a' && key != 's' && key != 'd')
@@ -149,18 +156,26 @@ void move_hero(char hero[15][15], int row_hero, int col_hero, char hero1) {
 		case 'w':
 		case 'W':
 			row_hero -= 1;
+			hero[row_hero][col_hero] = hero1;
+			hero[row_hero + 1][col_hero] = '.';
 			break;
 		case 'a':
 		case 'A':
 			col_hero -= 1;
+			hero[row_hero][col_hero] = hero1;
+			hero[row_hero][col_hero + 1] = '.';
 			break;
 		case 's':
 		case 'S':
 			row_hero += 1;
+			hero[row_hero][col_hero] = hero1;
+			hero[row_hero - 1][col_hero] = '.';
 			break;
 		case 'd':
 		case 'D':
 			col_hero += 1;
+			hero[row_hero][col_hero = hero1] = hero1;
+			hero[row_hero][col_hero - 1] = '.';
 			break;
 		}
 		if (1)
