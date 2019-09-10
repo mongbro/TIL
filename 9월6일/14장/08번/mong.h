a2e295401f9b00b4dc73797d5d2e1f3722ed3a95
whd4607@naver.com <whd4607@naver.com>
2019-09-10 21:55:28 +0900

    ADD C Project

--- 9월6일/14장/08번/mong.h
+++ 9월6일/14장/08번/mong.h
@@ -1,1 +1,22 @@
 #pragma once
+#include<stdio.h>
+
+void array_copy(int src[][3], int dst[][3]);
+void print_array(int a[][3]);
+
+void array_copy(int src[][3], int dst[][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			*(*(dst + i) + j) = *(*(src + i) + j);
+		}
+	}
+}
+
+void print_array(int a[][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%3d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
