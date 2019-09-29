a0028440d821811c668977edab8a286150ba0ab6
whd4607@naver.com <whd4607@naver.com>
2019-09-29 19:13:50 +0900

    ADD Game Project

--- C/학술제/기본모델/기본모델/item.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<stdio.h>
-
-typedef struct item {
-	char name[20];
-	int eff1=0;
-	int eff2=0;
-}ITEM;
--- C/학술제/기본모델/기본모델/skill.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<stdio.h>
-
-typedef struct skill {
-	char name[20];
-	int eff1 = 0;
-	int eff2 = 0;
-}SKILL;
