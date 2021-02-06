8c63ccbca75cc016e3babeea7d03df014d019cbb
azml142 <whd4607@naver.com>
2021-02-06 19:24:11 +0900

    2월 6일

---
+++ PYTHON/2월 1일/4.3-1.py
@@ -0,0 +1,44 @@
+n, m = map(int, input().split())
+visited = [[0] * m for _ in range(n)]
+x, y, d = map(int, input().split())
+visited[x][y] = 1
+array = []
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+def turn_left():
+    global d
+    d -= 1
+    if d == -1:
+        d = 3
+
+count = 1
+turn_count = 0
+
+while True:
+    turn_left()
+    nx = x + dx[d]
+    ny = y + dy[d]
+    if array[nx][ny] == 0 and visited[nx][ny] == 0:
+        x, y = nx, ny
+        visited[x][y] = 1
+        count += 1
+        turn_count = 0
+        continue
+    else:
+        turn_count += 1
+    if turn_count == 4:
+        nx = x - dx[d]
+        ny = y - dy[d]
+        if array[nx][ny] == 0:
+            x, y = nx, ny
+            turn_count = 0
+            continue
+        else:
+            break
+
+print(count)
