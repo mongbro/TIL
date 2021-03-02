d09217b328e6791b967f0ea338a2a703be414182
JongEun Kim <whd4607@naver.com>
2021-03-02 19:24:35 +0900

    3월 2일

---
+++ PYTHON/3월 2일/5-2 BFS 예제.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7],
+]
+
+visited = [False] * 9
+
+def bfs(graph, start, visited):
+    qu = deque([start])
+    visited[start] = True
+    while qu:
+        v = qu.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                qu.append(i)
+                visited[i] = True
+
+bfs(graph, 1, visited)
---
+++ PYTHON/3월 2일/5.4-1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+n, m = map(int, input().split())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def bfs(x, y):
+    qu = deque()
+    qu.append((x, y))
+    while qu:
+        x, y = qu.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if arr[nx][ny] == 0:
+                continue
+            if arr[nx][ny] == 1:
+                if nx == 0 and ny == 0:
+                    continue
+                arr[nx][ny] = arr[x][y] + 1
+                qu.append((nx, ny))
+    return arr[n - 1][m - 1]
+
+print(bfs(0, 0))
+for i in range(n):
+    for j in range(m):
+        print('%2d' % arr[i][j], end=' ')
+    print()
